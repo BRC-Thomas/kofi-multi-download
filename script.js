7161b23895f141a0e8001614cda4de4986131b6e
Thomas <123388670+BRC-Thomas@users.noreply.github.com>
2025-08-03 11:51:37 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get all links from download page
+const links = [...document.querySelectorAll('a[href*="/api/file-upload/"]')];
+
+// For each link, create an temporary <a> & set the link
+links.forEach((link, i) => {
+  const a = document.createElement('a');
+  a.href = link.href;
+  a.download = '';
+  document.body.appendChild(a);
+  
+  // Add timeout
+  setTimeout(() => {
+    a.click(); // start download
+    a.remove(); // remove the temporary <a>
+  }, i * 200); // 200ms delay
+});
